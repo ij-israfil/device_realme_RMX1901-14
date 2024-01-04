Atharva-Agnihotri7115/android_device_realme_RMX1901

RMX1901: Initial device tree from RMX1921
Signed-off-by: Ij-Israfil <israfilkhan494@gmail.com>

AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1921.mk
+    $(LOCAL_DIR)/evolution_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1921-userdebug \
-    evolution_RMX1921-eng
+    evolution_RMX1901-userdebug \
+    evolution_RMX1901-eng

ij-israfil/device_realme_RMX1901-14

AndroidProducts.mk
@@ -1,12 +1,12 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_RMX1921.mk
+    $(LOCAL_DIR)/aosp_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_RMX1921-userdebug \
-    evolution_RMX1921-eng
+    aosp_RMX1901-userdebug \
+    aosp_RMX1901-eng